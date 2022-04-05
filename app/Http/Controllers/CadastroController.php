3ffa99b93d12688ca420e570458e747ef4eeb8b1
Dayvson Timóteo <dayvson.timoteo@aol.com>
2022-04-05 13:16:57 -0300

    O formulário agora é enviado para o banco pelo método post

--- app/Http/Controllers/CadastroController.php
+++ app/Http/Controllers/CadastroController.php
@@ -3,26 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\AppCadastro;
+
 
 class CadastroController extends Controller
 {
-    public function cadastro(){
+    public function cadastro(Request $request){
+        // echo '<pre>';
+        // print_r($request->all());
+        // echo $request->input('campo')
+        // echo '</pre>';
+        if(!empty($cadastro)){
+            $cadastro = new AppCadastro();
+            $cadastro->tipo = $request->input('tipo');
+            $cadastro->numero = $request->input('numero');
+            $cadastro->unidade = $request->input('unidade');
+            $cadastro->data = $request->input('data');
+            $cadastro->linkPdf = $request->input('linkPdf');
+            $cadastro->conteudo = $request->input('conteudo');
+            
+            $cadastro->save();
+        }
         return view('site.cadastro');
-    }
-
-    public function txtPreview($doc, $search){
-        $doc = strtolower($doc);
-        $doc = explode(" ",$doc);
-        $wordIndice =array_search($search, $doc);
-        if ($wordIndice >=250) {
-            $doc = array_slice($doc, $wordIndice - 249, 500);
-        }
-        else{
-            $doc = array_slice($doc, $wordIndice, 500);
-        }
-        $doc = str_ireplace($search, "<strong>" . $search . "</strong>", $doc);
-        $doc = implode(" ", $doc);
-        return $doc;
     }
 }
 
